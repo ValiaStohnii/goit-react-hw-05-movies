38ac8445afb827e6b40473155d485ef1fcd09fb0
ValiaStohnii <valia19961018@gmail.com>
2022-01-24 01:03:43 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import AppBar from './components/AppBar/AppBar';
+import HomePage from './components/View/HomePage/HomePage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<AppBar />}>
+          <Route element={<HomePage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
