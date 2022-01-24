1ebb2f37698c2be7798b7282a84df4e977d0225d
ValiaStohnii <valia19961018@gmail.com>
2022-01-25 00:26:40 +0200

    add homePage

--- src/components/View/HomePage/HomePage.js
+++ src/components/View/HomePage/HomePage.js
@@ -1,3 +1,17 @@
+import { useState, useEffect } from 'react';
+import * as moviesApi from '../../services/Movies-api';
+
 export default function HomePage() {
-  return <h1>HomePage</h1>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    moviesApi.fetchTrendingMovies().then(setMovies);
+  }, []);
+
+  return (
+    <div>
+      <h1>HomePage</h1>
+      {movies && movies.map(movie => <li key={movie.id}>{movie.original_title}</li>)}
+    </div>
+  );
 }
